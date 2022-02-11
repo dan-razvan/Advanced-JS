d149c59784c4c7119e343c78a2d4c4ff043a180d
Razvan Bogdan <razvanbogdanturiatca@gmail.com>
2022-02-12 00:44:20 +0200

    cleaned unused code from css file and movie file

--- Movie App API/src/domain/Movie.ts
+++ Movie App API/src/domain/Movie.ts
@@ -28,29 +28,10 @@ export default class Movie {
   }
 }
 
-// export class MovieDataMapper {
-    
-//     map(data, cb) {
-//         let movies = []
-//         data.results.forEach( movie => {
-//           const movieObj = new Movie(movie.id, movie.title, movie.vote_average, movie.release_date.substring(0,4), movie.poster_path, movie.original_language)
-//           movies.push(movieObj)
-//         })
-//         console.log(movies)
-//         cb(movies)
-        
-//     }
-// }
-
 export class MovieDataMapper {
-    // movies: [];
-    // constructor(movies:[]) {
-    //     this.movies = movies;
-    // }
+
     map(data, cb) {
-    
-        // Data mapping
-        // object plain -> hydration -> object Movie
+
         let movies = [];
         data.results.forEach(element => {
             const movieData = element;
